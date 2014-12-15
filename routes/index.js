4113a62cf10ffc2c976760a7dac13d7e7f5c06a2
cttibbetts <cttibbetts@wpi.edu>
2014-12-15 15:38:21 -0600

    Basic templating and styling

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', function(req, res) {
+    res.render('home')
+});
+
 module.exports = router;
